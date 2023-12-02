29a6fafd740fde5eeb0bcf686d7f394f74cffbf9
Lemuel K <kyenyarkolemuel@gmail.com>
2023-12-02 14:40:57 -0500

    added a search box

--- vplaylistor/src/App.tsx
+++ vplaylistor/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Banner from './components/Banner/Banner';
+import TextBox from './components/TextBox/TextBox'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Banner message='' backgroundColor='#3498db'textColor='#000' />
-      
+      <Banner message='' backgroundColor='#3498db' textColor='#000'/>
+      <TextBox/>
       </header>
-      
     </div>
   );
 }
